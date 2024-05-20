24c2c021c65b29816f211074872fc6ce6bded41a
afifihsnm <afifihsanke2@gmail.com>
2024-05-20 22:38:24 +0700

    add all database migration

---
+++ app/Models/StoreTransaction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class StoreTransaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'waste_bank_id',
+        'total',
+        'transaction_type',
+        'address',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function wasteBank(): BelongsTo
+    {
+        return $this->belongsTo(WasteBank::class);
+    }
+
+    public function storeTransactionDetails(): HasMany
+    {
+        return $this->hasMany(StoreTransactionDetail::class);
+    }
+}
